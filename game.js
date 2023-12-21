5d3ac1129bfe30e790d455569706c97892017607
Takashi Tsuruoka <tsuruoka91@gmail.com>
2023-12-21 22:58:08 +0900

    タッチパネル対応

--- game.js
+++ game.js
@@ -123,21 +123,25 @@ var kumoImage;
 })();
 
 
-// マウス移動
-canvas.addEventListener("mousemove", (e) => {
+// マウス、タッチ移動
+function move(e) {
     var rect = e.target.getBoundingClientRect();
     const x = e.clientX - rect.left;
     cloudX = x;
     if(cloudX < BORDER + 35) cloudX = BORDER + 35;
     if(cloudX > WIDTH - 55) cloudX = WIDTH - 55;
-}, false);
+}
+canvas.addEventListener("mousemove", move, false);
+canvas.addEventListener("touchmove", move, false);
 
-// マウスがクリックされたら
-canvas.addEventListener("click", (e) => {
+// マウスクリック、タッチされたら
+function click(e) {
     addFruits(cloudX, OFFSET_Y - 50, nowNo);
     nowNo = nextNo;
     nextNo = Math.floor(Math.random() * 4);
-}, false);
+}
+canvas.addEventListener("click", click, false);
+canvas.addEventListener("touchend", click, false);
 
 // フルーツを追加する
 function addFruits(x, y, no){
